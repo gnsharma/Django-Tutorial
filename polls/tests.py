37d67f30994b0f7263f9a91a1de3eb049e216fdb
Govind N Sharma <gnsharma0810@gmail.com>
2018-01-29 12:12:10 +0530

    fixed all flake8 errors using autopep8

--- polls/tests.py
+++ polls/tests.py
@@ -9,8 +9,8 @@ from .models import Question
 
 def create_question(question_text, days):
     """
-    Create a question with the given 'question_text' and published the 
-    given number of 'days' offset to now (negative for questions published 
+    Create a question with the given 'question_text' and published the
+    given number of 'days' offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
     time = timezone.now() + datetime.timedelta(days=days)
